3d6df9f1fb2e5cd10cf584dc21a113ac3e508fea
pragati-jpg <85781824+pragati-jpg@users.noreply.github.com>
2021-08-15 14:01:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var Computerb;
+var Playerb;
+var Player;
+var Computerp;
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+   //Initialising Engine
+  engine = Engine.create();
+  world = engine.world;
+	
+   //Create Player Base and Computer Base Object
+Computerb = new computerbase(width-300,random(450,height-300),180,150)
+Playerb = new Playerbase(300,random(450,height-300),180,150)
+
+Player = new player(285,Playerb.body.position.y-153,50,180)
+Computerp = new computer(width-280,Computerb.body.position.y-153,50,180)
+ }
+
+function draw() {
+
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+   //Display Playerbase and computer base 
+Computerb.display();
+Playerb.display();
+   //display Player and computerplayer
+Player.display()
+Computerp.display()
+}
